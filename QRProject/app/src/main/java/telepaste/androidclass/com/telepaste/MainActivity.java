af10512ef47d4854ce3b4202093a10890e768cb3
Trần Ngọc Sơn <transon821996@gmail.com>
2017-06-03 21:56:59 +0700

    update1
    read qr code

--- QRProject/app/src/main/java/telepaste/androidclass/com/telepaste/MainActivity.java
+++ QRProject/app/src/main/java/telepaste/androidclass/com/telepaste/MainActivity.java
@@ -1,13 +1,211 @@
 package telepaste.androidclass.com.telepaste;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+    private ImageView imageView;
+    private ImageView img_flash;
+    private Camera camera;
+    private Parameters parameter;
+    private boolean deviceHasFlash;
+    private boolean isFlashLightOn = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=(ImageView) findViewById(R.id.img_background);
+        img_flash=(ImageView) findViewById(R.id.img_flash);
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        Bitmap blurred = blurRenderScript(bitmap,25);//second parametre is radius
+        imageView.setImageBitmap(blurred);
+
+        deviceHasFlash = getApplication().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if(!deviceHasFlash){
+            Toast.makeText(MainActivity.this, "Sorry, you device does not have any camera", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else{
+            this.camera = Camera.open(0);
+            parameter = this.camera.getParameters();
+        }
+        onFlash();
+
+    }
+
+    private Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
+    }
+
+    public void QrScanner(View view){
+
+
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();         // Start camera
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+    private void onFlash() {
+        img_flash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isFlashLightOn){
+                    img_flash.setImageResource(R.drawable.ic_flash_off);
+                    turnOffTheFlash();
+                }else if(!isFlashLightOn){
+                    img_flash.setImageResource(R.drawable.ic_flash_on);
+                    turnOnTheFlash();
+                }
+            }
+        });
+    }
+    private void turnOffTheFlash() {
+        parameter.setFlashMode(Parameters.FLASH_MODE_OFF);
+        this.camera.setParameters(parameter);
+        this.camera.stopPreview();
+        isFlashLightOn = false;
+    }
+
+    private void turnOnTheFlash() {
+        if(this.camera != null){
+            parameter = this.camera.getParameters();
+            parameter.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            this.camera.setParameters(parameter);
+            this.camera.startPreview();
+            isFlashLightOn = true;
+        }
+    }
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                parameter = camera.getParameters();
+            } catch (RuntimeException e) {
+                System.out.println("Error: Failed to Open: " + e.getMessage());
+            }
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(this.camera != null){
+            this.camera.release();
+            this.camera = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(deviceHasFlash){
+            Log.e("scorpion","onResume");
+            turnOffTheFlash();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        // Do something with the result here
+
+        Log.e("handler",result.getText()); // Prints scan results
+        Log.e("handler",result.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
+    }
+    private Bitmap blurRenderScript(Bitmap smallBitmap, int radius) {
+
+        try {
+            smallBitmap = RGB565toARGB888(smallBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                smallBitmap.getWidth(), smallBitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        RenderScript renderScript = RenderScript.create(this);
+
+        Allocation blurInput = Allocation.createFromBitmap(renderScript, smallBitmap);
+        Allocation blurOutput = Allocation.createFromBitmap(renderScript, bitmap);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(renderScript,
+                Element.U8_4(renderScript));
+        blur.setInput(blurInput);
+        blur.setRadius(radius); // radius must be 0 < r <= 25
+        blur.forEach(blurOutput);
+
+        blurOutput.copyTo(bitmap);
+        renderScript.destroy();
+
+        return bitmap;
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("scorpion","onDestroy");
+        turnOffTheFlash();
     }
 }
+
